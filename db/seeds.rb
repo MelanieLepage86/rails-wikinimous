dd3ac339faa320f24f464715eab3c5328406289c
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-04 17:05:37 +0100

    Rails Wikinimous => Code OK, Front not yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do
+  article = Article.new(
+    title: Faker::Movies::HowToTrainYourDragon.character,
+    content: Faker::TvShows::BrooklynNineNine.quote
+  )
+  article.save
+end
